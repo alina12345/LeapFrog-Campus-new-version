9d8849ffdaf00d39c000aae487f9a2b418e3450d
alina <alina.shakya3@gmail.com>
2013-03-19 10:27:26 +0545

    Leapfrog

---
+++ index.php
@@ -0,0 +1,111 @@
+
+<html>
+    <head>
+
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="css/style.css"></link>
+        <link href="themes/1/js-image-slider.css" rel="stylesheet" type="text/css" />
+        <script src="themes/1/js-image-slider.js" type="text/javascript"></script>
+
+    </head>
+    <body>
+    <center>
+        <div class="body">
+            <table>
+                <td width="980px"><img src="images/greenlogo.png" alt="logo"></td>
+                <td id="search"><input type="text" name="text" placeholder="Search Here"></input></td>
+                <td><img src="images/search-button-without-text-md.png" height="35px" width="35px"></img></td>
+
+            </table>
+        </div>
+        <div class="menu">
+            <ul> 
+
+                <li><a href="#"><font color="#008500">Home</font></a></li>
+                <li><a href="#"><font color="#008500">About</font></a></li>
+                <li><a href="#"><font color="#008500">Programs</font></a></li>
+                <li><a href="#"><font color="#008500">Facilities</font></a></li>
+                <li><a href="#"><font color="#008500">Admission</font></a></li>
+                <li><a href="#"><font color="#008500">Downloads</font></a></li>
+                <li><a href="#"><font color="#008500">Gallery</font></a></li>
+                <li><a href="#"><font color="#008500">Contact Us</font></a></li>
+                </tr>
+
+            </ul>
+        </div>
+        <div class="part1">
+            <div class="slider">
+                <div class="div1">
+                </div>
+                <div id="sliderFrame">
+                    <div id="ribbon"></div>
+                    <div id="slider">
+                        <a href="http://www.menucool.com/jquery-slider" target="_blank">
+                            <img src="images/image-slider-1.jpg" alt="" />
+                        </a>
+                        <img src="images/image-slider-2.jpg" alt="" />
+                        <img src="images/image-slider-3.jpg" alt="" />
+                        <img src="images/image-slider-4.jpg" alt="" />
+                        <img src="images/image-slider-5.jpg" />
+                    </div>
+                    
+                </div>
+                
+            </div>
+            <div class="info">
+                <img src="images/info/departments.png"></img><br><br>
+                <a href="#"><font color="#008500">Departments</font></a><br><br>
+                <img src="images/info/contact_us.png"></img><br><br>
+                <a href="#"><font color="#008500">Contact Us</font></a><br><br>
+                <img src="images/info/photo_gallery.png"></img><br><br>
+                <a href="#"><font color="#008500">Photo Gallery</font></a>
+            </div>
+          
+        </div>
+        
+        <div class="part2">
+            
+            <div class="leftpart">
+                
+            </div>
+           
+            
+            <div class="rightpart">
+                
+            </div>
+            <div class="left">
+                <div class="lefttop">
+                    <h3>Login Form</h3><br>
+                    Username  :<input type="text" name="user" placeholder="Write your username"></input><br><br>
+                    Password  :<input type="password" name="pass" placeholder="Mention your password"></input><br><br>
+                    <input type="submit" name="submie" value="LOGIN"></input>
+                </div>
+                <div class="leftdown">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <ul> 
+
+                <li><a href="#"><font color="#008500">Home</font></a></li>
+                <li><a href="#"><font color="#008500">About</font></a></li>
+                <li><a href="#"><font color="#008500">Programs</font></a></li>
+                <li><a href="#"><font color="#008500">Facilities</font></a></li>
+                <li><a href="#"><font color="#008500">Admission</font></a></li>
+                <li><a href="#"><font color="#008500">Downloads</font></a></li>
+                <li><a href="#"><font color="#008500">Gallery</font></a></li>
+                <li><a href="#"><font color="#008500">Contact Us</font></a></li>
+                </tr>
+
+            </ul>
+            
+           <center> <img src="images/logo.png" height="30px"></img>
+               <br>
+               <font size="2px" color="#008500"><b>Copyright 2012, All Rights Reserved</b></font>
+           </center>
+        </div>
+    </center>
+</body>
+</body>
+</html>
