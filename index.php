9c3c0ff47fb944f593a24417a0878b81fe45362d
alina <alina.shakya3@gmail.com>
2013-03-20 09:15:14 +0545

    index.php edited

--- index.php
+++ index.php
@@ -10,23 +10,27 @@
     </head>
     <body>
     <center>
-        <div class="body">
-            <table>
-                <td width="980px"><img src="images/greenlogo.png" alt="logo"></td>
-                <td id="search"><input type="text" name="text" placeholder="Search Here"></input></td>
-                <td><img src="images/search-button-without-text-md.png" height="35px" width="35px"></img></td>
+        <div class="header">
+            <div id="header_left">
+                <img src="images/sp.png" alt="logo" align="left">
+            </div>
+            <div id="header_center">
+                <h4>Leapfrog College of Information Technology</h4>
+                <div id="hcenter_down">Tribhuvan University</div>
+            </div>
+            <div id="header_right">
+                <input id="search" type="text" name="text" placeholder="Search Here"></input>
+                <img src="images/search-button-without-text-md.png" height="25px" width="25px"></img>
+            </div>
 
-            </table>
         </div>
+        <div class="clear"></div>
         <div class="menu">
             <ul> 
 
                 <li><a href="#"><font color="#008500">Home</font></a></li>
-                <li><a href="#"><font color="#008500">About</font></a></li>
-                <li><a href="#"><font color="#008500">Programs</font></a></li>
-                <li><a href="#"><font color="#008500">Facilities</font></a></li>
-                <li><a href="#"><font color="#008500">Admission</font></a></li>
-                <li><a href="#"><font color="#008500">Downloads</font></a></li>
+                <li><a href="#"><font color="#008500">About Us</font></a></li>
+                <li><a href="#"><font color="#008500">Departments</font></a></li>
                 <li><a href="#"><font color="#008500">Gallery</font></a></li>
                 <li><a href="#"><font color="#008500">Contact Us</font></a></li>
                 </tr>
@@ -48,9 +52,9 @@
                         <img src="images/image-slider-4.jpg" alt="" />
                         <img src="images/image-slider-5.jpg" />
                     </div>
-                    
+
                 </div>
-                
+
             </div>
             <div class="info">
                 <img src="images/info/departments.png"></img><br><br>
@@ -60,28 +64,46 @@
                 <img src="images/info/photo_gallery.png"></img><br><br>
                 <a href="#"><font color="#008500">Photo Gallery</font></a>
             </div>
-          
+
         </div>
-        
+
         <div class="part2">
-            
+
             <div class="leftpart">
-                
+                <center><h3>Welcome</h3>
+                    <img src="images/college.jpg" alt="College Building" height="150px" width="150px"></img></center>
+                <p>Leapfrog Campus was established in 2006 A.D. under the affiliation of Tribhuvan University,on the chairmanship of Mr.Richan Shrestha with the aim of providing all-round education to the students. Along
+                </p>
+                <input id="readmore" type="button" name="button" value="Read More&gt&gt;"></input>
             </div>
-           
-            
+
+
             <div class="rightpart">
-                
+                <center><h3>Message from the Principal</h3>
+                    <img src="images/himalsir.jpeg" alt="Himal Karmacharya" height="150px" width="150px"></img></center>
+                <p>It has been a matter of immense pleasure for me on mentioning that Leapfrog Campus has completed six glorious years of success and has proved to be the leading educational institution across the nation.
+                </p>
+                <input id="readmore" type="button" name="button" value="Read More&gt&gt;"></input>
             </div>
             <div class="left">
-                <div class="lefttop">
-                    <h3>Login Form</h3><br>
-                    Username  :<input type="text" name="user" placeholder="Write your username"></input><br><br>
-                    Password  :<input type="password" name="pass" placeholder="Mention your password"></input><br><br>
-                    <input type="submit" name="submie" value="LOGIN"></input>
+                <div class="right_round">
+                    <table style="width:250;border:2px solid #008500;"><tr><td align="center"><font color="#008500" size="3px"><b>Login Form</b></font></td></tr></table>
+                    <table style=" width:250;border:1px solid #008500;" >
+
+                        <tr><td>Username</td>
+                            <td><input type="text" style="width:150px; height:25px; font-size:12px;" name="user" placeholder="Write your username"></input></td>
+                        </tr>
+                        <tr>
+                            <td>Password</td>
+                            <td><input type="password" style="width:150px; height:25px; font-size:12px;" name="pass" placeholder="Mention your password"></input></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><input type="submit" name="submit" value="Login"></input></td>
+                    </table>
                 </div>
                 <div class="leftdown">
-                    
+
                 </div>
             </div>
         </div>
@@ -99,11 +121,11 @@
                 </tr>
 
             </ul>
-            
-           <center> <img src="images/logo.png" height="30px"></img>
-               <br>
-               <font size="2px" color="#008500"><b>Copyright 2012, All Rights Reserved</b></font>
-           </center>
+
+            <center> <img src="images/logo.png" height="30px"></img>
+                <br>
+                <font size="2px" color="#008500"><b>Copyright 2012, All Rights Reserved</b></font>
+            </center>
         </div>
     </center>
 </body>
